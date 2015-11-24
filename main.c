a5f93b1b5d3d1c48b0aac6493cfa36d34ea00ac8
robm123 <roberto.merino85@gmail.com>
2015-11-24 00:10:13 -0500

    created main

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "myMalloc.h"
+
+
+
+main()
+{
+		
+	int* ptr = (int*)malloc(2000);
+	int* ptr1 = (int*)malloc(2000);
+	int* ptr2 = (int*)malloc(1000);
+	
+	
+//	int* ptr3 = (int*)malloc(1000);
+//	int* ptr4 = (int*)malloc(1000);
+	
+
+//	int x;
+//	free(&x);
+	
+
+	return 0;
+}
